2e19af274ff5e61211fb3f62fd6e97e39adc04ef
Sumanth <66694715+Sumanth077@users.noreply.github.com>
2023-08-13 14:30:28 +0530

    Create Python.py

---
+++ Claude Instant 1.2/Python.py
@@ -0,0 +1,62 @@
+
+######################################################################################################
+# In this section, we set the user authentication, user and app ID, model details, and the URL of 
+# the text we want as an input. Change these strings to run your own example.
+######################################################################################################
+
+# Your PAT (Personal Access Token) can be found in the portal under Authentification
+PAT = ''
+# Specify the correct user_id/app_id pairings
+# Since you're making inferences outside your app's scope
+USER_ID = 'anthropic'
+APP_ID = 'completion'
+# Change these to whatever model and text URL you want to use
+MODEL_ID = 'claude-instant-1_2'
+MODEL_VERSION_ID = '710652a9f5c0439086c4dd96170c596a'
+TEXT_FILE_URL = 'https://samples.clarifai.com/negative_sentence_12.txt'
+
+############################################################################
+# YOU DO NOT NEED TO CHANGE ANYTHING BELOW THIS LINE TO RUN THIS EXAMPLE
+############################################################################
+
+from clarifai_grpc.channel.clarifai_channel import ClarifaiChannel
+from clarifai_grpc.grpc.api import resources_pb2, service_pb2, service_pb2_grpc
+from clarifai_grpc.grpc.api.status import status_code_pb2
+
+channel = ClarifaiChannel.get_grpc_channel()
+stub = service_pb2_grpc.V2Stub(channel)
+
+metadata = (('authorization', 'Key ' + PAT),)
+
+userDataObject = resources_pb2.UserAppIDSet(user_id=USER_ID, app_id=APP_ID)
+
+post_model_outputs_response = stub.PostModelOutputs(
+    service_pb2.PostModelOutputsRequest(
+        user_app_id=userDataObject,  # The userDataObject is created in the overview and is required when using a PAT
+        model_id=MODEL_ID,
+        version_id=MODEL_VERSION_ID,  # This is optional. Defaults to the latest model version
+        inputs=[
+            resources_pb2.Input(
+                data=resources_pb2.Data(
+                    text=resources_pb2.Text(
+                        url=TEXT_FILE_URL
+                    )
+                )
+            )
+        ]
+    ),
+    metadata=metadata
+)
+if post_model_outputs_response.status.code != status_code_pb2.SUCCESS:
+    print(post_model_outputs_response.status)
+    raise Exception("Post model outputs failed, status: " + post_model_outputs_response.status.description)
+
+# Since we have one input, one output will exist here
+output = post_model_outputs_response.outputs[0]
+
+print("Predicted concepts:")
+for concept in output.data.concepts:
+    print("%s %.2f" % (concept.name, concept.value))
+
+# Uncomment this line to print the full Response JSON
+#print(output)
